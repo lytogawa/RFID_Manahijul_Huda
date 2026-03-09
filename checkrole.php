6121a7e4dee9bc619a8670ddaeb913167464ac4a
Gatot Santoso <santosogatot88@gmail.com>
2026-03-09 14:21:28 +0700

    Create checkrole.php

---
+++ checkrole.php
@@ -0,0 +1,24 @@
+// app/Http/Middleware/CheckRole.php
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckRole
+{
+    public function handle(Request $request, Closure $next, string ...$roles): Response
+    {
+        if (!$request->user()) {
+            return redirect()->route('login');
+        }
+
+        if (!in_array($request->user()->role, $roles)) {
+            abort(403, 'Unauthorized access');
+        }
+
+        return $next($request);
+    }
+}
