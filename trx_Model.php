8f5eecf56ddc78019d1d7be7454802e3fe7c5f7e
Gatot Santoso <santosogatot88@gmail.com>
2026-03-09 14:19:10 +0700

    Create trx_Model.php

---
+++ trx_Model.php
@@ -0,0 +1,25 @@
+// app/Models/Transaction.php
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['account_id', 'amount', 'description', 'type', 'location'];
+
+    public function account(): BelongsTo
+    {
+        return $this->belongsTo(Account::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsToThrough(User::class, Account::class);
+    }
+}
